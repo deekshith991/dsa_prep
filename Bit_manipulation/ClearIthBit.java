0b0b6e2147480bde96391eaa2f774172b5bb4e21
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-06 21:34:31 +0530

    Update: Clear Ith Bit

--- Bit_manipulation/ClearIthBit.java
+++ Bit_manipulation/ClearIthBit.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 /**
  * ResetIthBit
  */
-public class ResetIthBit {
+public class ClearIthBit {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int i = sc.nextInt();
         sc.close();
+
+        System.out.println(ClearBit(n,i));
+
+    }
+
+    public static int ClearBit( int n , int i){
         
+        return (n&(~(1<<i)));
     }
 }
