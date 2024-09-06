c6795a9bc1288cc2e081592d73e92c974621f69c
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-07 00:41:59 +0530

    change to checks

--- Bit_manipulation/Integer2Binary.java
+++ Bit_manipulation/Integer2Binary.java
@@ -18,14 +18,17 @@ public class Integer2Binary {
 
     public static String convert( int n) {
         
-        StringBuffer res = new StringBuffer(); 
+        StringBuffer res = new StringBuffer();
+        if(n==0) return "0";
         while( n != 0){
-            if( n%2 == 1){
+            if( (n&1) == 1){
                 res.append(1);
             }else{
                 res.append(0);
             }
-            n=n/2;
+            n = n>>1;
+
+            //System.out.println(n);
         }
         //res.append(1);
 
