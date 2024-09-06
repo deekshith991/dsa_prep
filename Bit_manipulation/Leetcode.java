7e31f0187869f0c411183599bc57a1f7ee70a53a
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-07 00:43:06 +0530

    Update: leetcode template

---
+++ Bit_manipulation/Leetcode.java
@@ -0,0 +1,33 @@
+
+
+package Bit_manipulation;
+import java.util.Scanner;
+/**
+ * Leetcode
+ */
+public class Leetcode {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+        int[] arr = {1,3,4,2,2};
+
+        Solution s = new Solution();
+        s.findDuplicate(arr);
+    }
+
+    
+}
+
+class Solution {
+    /**
+     * Need to do it Floyd's Hare and tortoise Algorith.
+     *
+     * or find a new method ;
+     * */
+    public int findDuplicate(int[] nums) {
+
+        return 0;
+    }
+}
