78e4c852a651ba5409f15dd08b96253dcd00ec2a
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-09-14 21:54:57 +0530

    kadene's Algo Done

--- Algorithms/Arrays/Kadane.java
+++ Algorithms/Arrays/Kadane.java
@@ -1,5 +1,6 @@
 
 package Algorithms.Arrays;
+import Utility.io;
 
 /**
  * Kadane
@@ -7,6 +8,24 @@ package Algorithms.Arrays;
 public class Kadane {
 
   public static void main(String[] args) {
-    
+    int[] arr = io.randArr();
+    io.display(arr);
+
+    System.out.println( solution(arr) );
+  }
+
+  public static int solution(int[] arr){
+
+    int maxi = Integer.MIN_VALUE;
+    int sum =0;
+    for (int i = 0; i < arr.length; i++) {
+      sum += arr[i];
+      if( sum > maxi)
+        maxi = sum;
+
+      if( sum < 0)
+        sum = 0;
+    }
+    return sum ;
   }
 }
